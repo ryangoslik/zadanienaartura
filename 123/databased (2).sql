f43cceb3ca768a52d02c2d63dc90918fe0c33197
unknown <konrad.bienkowski1@o2.pl>
2024-02-14 22:25:31 +0100

    123

---
+++ 123/databased (2).sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 14, 2024 at 10:24 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `databased`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `komentarze`
+--
+
+CREATE TABLE `komentarze` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `tresc` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `komentarze`
+--
+
+INSERT INTO `komentarze` (`id`, `login`, `tresc`) VALUES
+(3, 'Kanye West', 'Im good at ping pong'),
+(6, 'kanyelover911', 'I miss the old kanye...');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `rejestracja`
+--
+
+CREATE TABLE `rejestracja` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `haslo` varchar(255) NOT NULL,
+  `Komentarze` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rejestracja`
+--
+
+INSERT INTO `rejestracja` (`id`, `login`, `haslo`, `Komentarze`) VALUES
+(1, 'artwoj', '951963987', ''),
+(25, 'kanyelover911', '123', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uczen`
+--
+
+CREATE TABLE `uczen` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(255) NOT NULL,
+  `nazwisko` varchar(255) NOT NULL,
+  `comments` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `uczen`
+--
+
+INSERT INTO `uczen` (`id`, `imie`, `nazwisko`, `comments`) VALUES
+(1, 'Artur', 'Wojciech', ''),
+(30, 'Kanye', 'West', '');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `komentarze`
+--
+ALTER TABLE `komentarze`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `rejestracja`
+--
+ALTER TABLE `rejestracja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `uczen`
+--
+ALTER TABLE `uczen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `komentarze`
+--
+ALTER TABLE `komentarze`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `rejestracja`
+--
+ALTER TABLE `rejestracja`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `uczen`
+--
+ALTER TABLE `uczen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
